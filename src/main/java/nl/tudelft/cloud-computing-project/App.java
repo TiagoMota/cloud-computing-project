25129d8a5b4a2696c586da4a5c575173c656861d
Widar91 <eddy.bertoluzzo@gmail.com>
2013-10-18 00:47:46 +0200

    empty maven project setup

---
+++ src/main/java/nl/tudelft/cloud-computing-project/App.java
@@ -0,0 +1,13 @@
+//package nl.tudelft.cloud-computing-project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
