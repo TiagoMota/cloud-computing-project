12dc141dc75b2bca5f17047c50a36d96909784e5
David Hoepelman <dhoepelman@gmail.com>
2013-10-30 00:55:20 +0100

    Test files

---
+++ src/test/java/nl/tudelft/cloud_computing_project/setup/SetupTestDataSet.java
@@ -0,0 +1,12 @@
+package nl.tudelft.cloud_computing_project.setup;
+
+import nl.tudelft.cloud_computing_project.model.Database;
+
+public class SetupTestDataSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
