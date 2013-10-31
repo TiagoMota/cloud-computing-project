dede838c68d4d04a6574ad15f97d6c7e53a6d7aa
David Hoepelman <dhoepelman@gmail.com>
2013-10-31 12:36:47 +0100

    Reset query cache

--- src/main/java/nl/tudelft/cloud_computing_project/experimental/dbbenchmarks/measure.php
+++ src/main/java/nl/tudelft/cloud_computing_project/experimental/dbbenchmarks/measure.php
@@ -1,11 +1,12 @@
 <?php
 
-$q = "SELECT id, filesize FROM Job WHERE jobstatus = 1 AND id NOT IN (SELECT job_id FROM Assignment) ORDER BY priority, submission_time";
+$q = " SELECT id, filesize FROM Job WHERE jobstatus = 1 AND id NOT IN (SELECT job_id FROM Assignment) ORDER BY priority, submission_time";
 
 $c= mysql_connect($argv[1], "cloudocr", "cloudocr");
 mysql_select_db("cloudocr_db");
 
 for($i = 0; $i < $argv[2]; $i++) {
+        mysql_query("RESET QUERY CACHE;");
         $start = microtime(true);
 
         $r = mysql_query($q);
